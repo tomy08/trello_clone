6c1c6c0c0f51f20ee6007c35ce3e99f47c26ec1b
tomy08 <tomasesantacruz@gmail.com>
2025-12-03 11:27:28 -0300

    feat: integrate JWTManager into the Flask application and update model exports

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from config import Config
 from src.db import db
 from src.routes import boards_bp, auth_bp, cards_bp, lists_bp
+from flask_jwt_extended import JWTManager
 
 
 def create_app():
@@ -13,6 +14,7 @@ def create_app():
 
     db.init_app(app)
     Migrate(app, db)
+    JWTManager(app)
 
     app.register_blueprint(boards_bp)
     app.register_blueprint(auth_bp)
--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,5 +1,7 @@
 from .board import Board
 from .board_member import BoardMember
 from .user import User
+from .card import Card
+from .list import List
 
-__all__ = ["Board", "BoardMember", "User"]
+__all__ = ["Board", "BoardMember", "User", "Card", "List"]
